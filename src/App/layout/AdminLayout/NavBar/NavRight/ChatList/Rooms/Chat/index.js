c3785c19832e2ef76b018ae9837dc9ab26ba2f82
Quân Nguyễn Anh <anhquan310399@gmail.com>
2021-04-25 13:01:00 +0700

    update messages 25/04/2021

--- src/App/layout/AdminLayout/NavBar/NavRight/ChatList/Rooms/Chat/index.js
+++ src/App/layout/AdminLayout/NavBar/NavRight/ChatList/Rooms/Chat/index.js
@@ -6,14 +6,19 @@ import { getCookie, getLocalStorage } from '../../../../../../../../services/loc
 import Message from './Message';
 import Aux from "../../../../../../../../hoc/_Aux";
 import DEMO from "../../../../../../../../store/constant";
+import { Spin, Row, Col } from 'antd';
+
+import 'antd/dist/antd.css';
 
 const ChatRoom = (props) => {
     const { socket } = props;
     const { room } = props;
     const [messageList, setMessageList] = useState([]);
     const currentUser = JSON.parse(getLocalStorage("user"));
+    const [isLoadMessages, setLoadMessages] = useState(false);
 
     const getMessages = () => {
+        setLoadMessages(true);
         axios.get(`${process.env.REACT_APP_API_URL}/chatroom/${room._id}`, {
             headers: {
                 'Authorization': getCookie("token")
@@ -21,28 +26,63 @@ const ChatRoom = (props) => {
         })
             .then(res => {
                 setMessageList(res.data.room.messages);
+                scrollToBottom();
             })
             .catch(error => {
                 console.log(error);
+            }).finally(() => {
+                setLoadMessages(false);
             })
     }
+
+    const [scrollCtrRef, setScrollCtrRef] = useState();
+    // eslint-disable-next-line no-unused-vars
+    const [scrollRef, setScrollRef] = useState();
 
     useEffect(() => {
         if (room._id) {
             getMessages();
             socket.emit('join-chat', { chatroomId: room._id });
-
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [room]);
+
+    const scrollToBottom = () => {
+        if (scrollCtrRef) {
+            scrollCtrRef.scrollTop = scrollCtrRef.scrollHeight;
+        }
+    }
+
+    const handleLoadMoreMessages = () => {
+        setLoadMessages(true);
+        axios.post(`${process.env.REACT_APP_API_URL}/chatroom/${room._id}/messages`,
+            {
+                current: messageList.length
+            },
+            {
+                headers: {
+                    'Authorization': getCookie("token")
+                }
+            })
+            .then(res => {
+                setMessageList([...res.data.messages, ...messageList]);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+            .finally(() => {
+                setLoadMessages(false);
+            })
+    }
 
     useEffect(() => {
         if (socket) {
             socket.on("newMessage", (message) => {
                 setMessageList([...messageList, message]);
+                scrollToBottom();
             })
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [messageList])
 
     const [messageDisplay, setMessageDisplay] = useState(null);
@@ -65,7 +105,7 @@ const ChatRoom = (props) => {
             });
             setMessageDisplay(message);
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [messageList])
 
     const [message, setMessage] = useState("");
@@ -100,8 +140,25 @@ const ChatRoom = (props) => {
                 </div>
                 <div className="h-list-body">
                     <div className="main-chat-cont">
-                        <PerfectScrollbar >
+                        <PerfectScrollbar
+                            onScrollUp={(ref) => {
+                                if (ref.scrollTop === 0) {
+                                    handleLoadMoreMessages();
+                                }
+                            }}
+                            ref={ref => {
+                                setScrollRef(ref);
+                            }}
+                            containerRef={ref => {
+                                setScrollCtrRef(ref);
+                            }}>
                             <div className="main-friend-chat">
+                                {isLoadMessages ?
+                                    (<Row justify="center">
+                                        <Spin tip="Loading..."></Spin>
+                                    </Row>) :
+                                    null
+                                }
                                 {messageDisplay}
                             </div>
                         </PerfectScrollbar>
@@ -113,8 +170,21 @@ const ChatRoom = (props) => {
                         <a href={DEMO.BLANK_LINK} className="input-group-prepend btn btn-success btn-attach">
                             <i className="feather icon-paperclip" />
                         </a>
-                        <input value={message} onChange={handleChangeMessage} type="text" name="h-chat-text" className="form-control h-send-chat" placeholder="Write hear . . " />
-                        <button onClick={sendMessage} type="submit" className="input-group-append btn-send btn btn-primary">
+                        <input
+                            onKeyDown={(e) => {
+                                if (e.key === 'Enter') {
+                                    sendMessage();
+                                }
+                            }}
+                            value={message}
+                            onChange={handleChangeMessage}
+                            type="text" name="h-chat-text"
+                            className="form-control h-send-chat"
+                            placeholder="Write hear . . " />
+                        <button
+                            onClick={sendMessage}
+                            type="submit"
+                            className="input-group-append btn-send btn btn-primary">
                             <i className="feather icon-message-circle" />
                         </button>
                     </div>
